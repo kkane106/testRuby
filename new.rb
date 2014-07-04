a232ca968a43b51b273b65d87b28bd9a302d5876
kkane106 <kkane106@gmail.com>
2014-07-03 22:55:46 -0400

    new code

---
+++ new.rb
@@ -0,0 +1,9 @@
+puts 'what\'s your first name?'
+f_name = gets.chomp
+puts 'what\'s your middle name?'
+m_name = gets.chomp
+puts 'what\'s your last name?'
+l_name = gets.chomp
+full_name = f_name + ' ' + m_name + ' ' + l_name
+puts 'your name is ' + full_name
+
